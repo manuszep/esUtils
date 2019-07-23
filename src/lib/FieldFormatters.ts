08fcc1e3cc41554a65a22f6ffd3a1b2bbf3f29f6
saulo pinto <pinto.sau@gmail.com>
2019-07-23 09:50:32 +0200

    add formating address

--- src/lib/FieldFormatters.ts
+++ src/lib/FieldFormatters.ts
@@ -1,3 +1,5 @@
+import { getTranslation } from "./components/TranslationComponent/index"
+
 export const priceFormatter = (value: string): string => {
   if (value === null || typeof value === "undefined") {
     return "";
@@ -9,3 +11,42 @@ export const priceFormatter = (value: string): string => {
 export const priceNormalizer = (value: string): string => {
   return `${value}`.replace(",", ".");
 };
+
+export const composeStreetAddress = (street: string, streetNr:string, boxNr: string): string => {
+  let addressStreetNr = "";
+  let addressBoxNr = boxNr;
+  if (streetNr) {
+    addressStreetNr = streetNr;
+  }
+
+  if (addressBoxNr !== "") {
+    addressBoxNr = ` ${getTranslation("POSTAL_BOX_TEXT")} ${addressBoxNr}`;
+  }
+
+  return `${street} ${addressStreetNr}${addressBoxNr}`.trim();
+}
+
+export const composeCityAddress = (postalCode:string, city:string): string => {
+  return `${postalCode} ${city}`;
+}
+
+export const composeFullAddress = (street:string, streetNr:string, boxNr: string, postalCode: string, city:string, oneLine:string): string => {
+  const separator = oneLine ? ", " : ",<br />";
+
+  const fullStreet = composeStreetAddress(street, streetNr, boxNr);
+  const fullCity = composeCityAddress(postalCode, city);
+
+  return `${fullStreet}${separator}${fullCity}`;
+}
+
+export const composeGmapsAddress = (street: string, streetNumber: string, postalCode: string, city: string) => {
+  let p1 = `${street} ${streetNumber}`;
+  let separator = ", ";
+
+  if (p1.length < 2) {
+    p1 = "";
+    separator = "";
+  }
+
+  return `${p1}${separator}${postalCode} ${city}`;
+}
