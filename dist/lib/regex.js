d58f0861ef831d94ec1a18dfbf1239e7f1191dcf
saulo pinto <pinto.sau@gmail.com>
2019-11-28 10:38:16 +0100

    remove files + build dist

--- dist/lib/regex.js
+++ dist/lib/regex.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.patternPhone = /^((\+|00)32\s?|0)(\d\s?\d{3}|\d{2}\s?\d{2})(\s?\d{2}){2}$/i;
-exports.patternPhoneMobile = /^((\+|00)32\s?|0)4(60|56|[789]\d)(\s?\d{2}){3}$/i;
+exports.patternPhoneMobile = /^((\+|00)32\s?|0)4(46|60|56|[789]\d)(\s?\d{2}){3}$/i;
 exports.patternEmail = /[a-zA-Z0-9!#$%&\'*+\=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&\'*+\=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-z0-9])?/;
