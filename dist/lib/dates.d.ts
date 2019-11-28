f2aff2cb68c2caf251e07c7da48ffa155b7b15d9
saulo pinto <pinto.sau@gmail.com>
2019-11-28 11:25:40 +0100

    modified

--- dist/lib/dates.d.ts
+++ dist/lib/dates.d.ts
@@ -2,5 +2,4 @@ import moment from "moment";
 export declare const parseDate: (date: string) => moment.Moment;
 export declare const getCurrentDate: () => moment.Moment;
 export declare const getDifferenceInYears: (firstDate: moment.Moment, secondDate: moment.Moment) => number;
-export declare const getAge: (dob: string, defaultAge: number) => number;
 export declare const getIsAdult: (dob: string) => boolean;
