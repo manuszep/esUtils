72f4d0f0bde2f4faa8a6f68affcd40233bcc5fe9
pisau <pinto.sau@gmail.com>
2020-03-06 09:28:27 +0100

    needHelp component

---
+++ dist/lib/components/EngagementCenter/needHelp.d.ts
@@ -0,0 +1,11 @@
+import { Component } from "react";
+import { KeyedObject } from "../../../index";
+declare class NeedHelp extends Component<KeyedObject, KeyedObject> {
+    const mapStateToProps: (state: KeyedObject<any>) => KeyedObject<any>;
+    getWhatsappUrl(): any;
+    needHelp(): void;
+    isCallCenterOpen(): Boolean;
+    renderNeedHelp(): "whatsapp__container" | undefined;
+    return(: any, div: any, className: any, : any, id: string | undefined, href: boolean | undefined, : any): any;
+}
+export default NeedHelp;
